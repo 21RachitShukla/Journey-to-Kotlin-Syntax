5980f5499bd438aaf82e0f964d9495c4279f4d92
Ankit Verma <ankitvermamat@gmail.com>
2018-06-16 01:08:16 +0530

    #2

---
+++ src/basics/_5_Function.kt
@@ -0,0 +1,41 @@
+package basics
+
+/**
+ * Created by avcbcoder on 16-06-2018.
+ */
+
+fun main(args: Array<String>) {
+    val a = 2
+    val b = 3
+    println(add2(a, b))
+    //After naming order doesn't matters
+    //Mixing is not allowed i.e either all params should be named or ordering should be maintained
+    struct(breadth = 12, length = 3, height = 1)
+
+    var ans = global { println("called lambda function from $it") }
+    println(ans(2, 3))
+}
+
+fun add1(a: Int, b: Int): Int {
+    return a + b
+}
+
+fun add2(a: Int, b: Int) = a + b
+
+fun max(a: Int, b: Int) = if (a > b) a else b
+
+//Concept of named params
+fun struct(length: Int, breadth: Int, height: Int) {
+    println(length)
+    println(breadth)
+    println(height)
+}
+
+fun global(l: (String) -> Unit): (Int, Int) -> (Int) {
+    l("global fun scope")
+    // Inner function can also be made in kotlin
+    fun inner(il: (String) -> Unit) = il("inner fun scope")
+    inner(l)
+    fun add(a: Int, b: Int): Int = a + b //.....?
+    return { a, b -> a + b }
+}
