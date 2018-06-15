fcd821398b4bcc9c5ebbeb6360a9ad974acf2433
Ankit Verma <ankitvermamat@gmail.com>
2018-06-16 01:33:08 +0530

    #5

---
+++ src/OOPS/_0_class.kt
@@ -0,0 +1,34 @@
+package OOPS
+
+
+/**
+ * Created by avcbcoder on 16-06-2018.
+ */
+fun main(args: Array<String>) {
+    var p = Person("Ankit")
+    p.display()
+
+    //data class
+    var s = Student("ankit", 20);
+    println(s.name + " " + s.age)
+
+}
+
+class Person {
+    var name: String
+
+    constructor(name: String) {
+        this.name = name
+    }
+
+
+    fun display() {
+        println(this.name)
+    }
+
+    override fun toString(): String {
+        return "Person(name='$name')"
+    }
+}
+
+data class Student(val name: String, val age: Int)
