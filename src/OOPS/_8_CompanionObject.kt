bb4514db3c4cdc8a9b6cbb1960eaab7e501a2872
Ankit Verma <ankitvermamat@gmail.com>
2018-06-16 01:41:41 +0530

    #7

---
+++ src/OOPS/_8_CompanionObject.kt
@@ -0,0 +1,20 @@
+package OOPS
+
+/**
+ * Created by avcbcoder on 16-06-2018.
+ */
+fun main(args: Array<String>) {
+    println(CompClass.staticField)
+    CompClass.staticFun()
+}
+
+class CompClass {
+    // After compilation, fields inside companion object actually becomes static fields of class
+    companion object { // These can be accessed using class name only , not by objects
+
+        var staticField: Int = 50
+
+        @JvmStatic //Using this, the function can now be accessed even from JAVA class
+        fun staticFun() = println("Actual static function")
+    }
+}
