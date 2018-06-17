0d3667ca393f2cf559233cf94d5b5b54b8164a26
Ankit Verma <ankitvermamat@gmail.com>
2018-06-17 19:40:16 +0530

    #9

---
+++ src/high_level_function/_2_Lambdas.kt
@@ -0,0 +1,18 @@
+package high_level_function
+
+/**
+ * Created by avcbcoder on 17-06-2018.
+ */
+
+fun main(args: Array<String>) {
+    // ............[...Signature...].......[Params]...[MethodBody]
+    val myLambda: (Int, Int) -> Int = { a, b -> a + b } // a=>Int,b=>Int , a+b will be returned
+
+    acceptLambda({ s: String, i: Int -> s + i })
+    //type is redundant
+    acceptLambda({ s, i -> s + i })
+    //Another syntax
+    acceptLambda(){ s, i -> s + i }
+}
+
+fun acceptLambda(lamdaFun: (String, Int) -> String) = println(lamdaFun("Hello", 55))
